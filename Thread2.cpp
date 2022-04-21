2b24593d413fe825c9c101efe5a8c7e2356928f3
Jesse Tennant <75157270+BeepBwop@users.noreply.github.com>
2022-04-20 19:00:51 -0700

    Update Thread2.cpp

--- Thread2.cpp
+++ Thread2.cpp
@@ -1,42 +1,99 @@
-void One(Object* obj) {
-    while (!obj->isThreeDone())
+#include <iostream>
+#include <string>
+#include <thread>
+#include <Windows.h>
+
+class Thing
+{
+    std::string mStr = " ";
+    int mNum = 0;
+
+public:
+
+    void operator()(int n, std::string s) //Functor
     {
-        obj->SetNum(17);
-        obj->SetWord("One");
+        this->mNum = n;
+        this->mStr = s;
     }
-}
-void Two(Object* obj) {
-    while (!obj->isThreeDone())
+
+    bool Timer = false;
+    void SetInt(int number) {mNum = number;}
+    int GetInt() {return mNum;}
+    void SetString(std::string string) {mStr = string; }
+    std::string GetString() { return mStr;}
+};
+
+
+struct OneCounter
+{
+    Thing* mPTR = nullptr;
+    void operator()()
     {
-        obj->SetNum(obj->GetNum() + 13);
-        obj->SetWord("Two");
-    }
-}
-void Three(Object* obj) {
-    unsigned int i = 0;
-    std::cout << i << std::endl;
-    while (i >= 0 && i != INT_MAX) {
-        if (i == INT_MAX) {
-            break;
+        while (mPTR->Timer != true)
+        {
+            mPTR->SetInt(17);
+            mPTR->SetString("One");
         }
-        obj->SetNum(i);
-        obj->SetWord("Three");
-        i++;
     }
-    obj->SetThreeDone(true);
-}
+};
+
+struct TwoCounter
+{
+    Thing* mPTR = nullptr;
+    void operator()()
+    {
+        while (mPTR->Timer != true)
+        {
+            mPTR->SetInt(mPTR->GetInt() + 13);
+            mPTR->SetString("Two");
+        }
+    }
+};
+
+struct ThreeCounter
+{
+    Thing* mPTR = nullptr;
+    void operator ()()
+    {
+        int ThreeCount = 0;
+        while (ThreeCount != INT_MAX)
+        {
+            if (mPTR->Timer == true)
+                break;
+
+            mPTR->SetInt(ThreeCount);
+            mPTR->SetString("Three");
+            ThreeCount++;
+        }
+ 
+    }
+};
+
 int main()
 {
-    Object* obj = new Object;
-    std::thread first(One, obj);
-    std::thread second(Two, obj);
-    std::thread third(Three, obj);
-    while (!obj->isThreeDone())
-    {
-        std::cout << obj->GetNum() << " " << obj->GetWord() << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-    first.join();
-    second.join();
-    third.join();
+    Thing* ThingPTR = new Thing;
+    OneCounter one;
+    one.mPTR = ThingPTR;
+    TwoCounter two;
+    two.mPTR = ThingPTR;
+    ThreeCounter three;
+    three.mPTR = ThingPTR;
+
+    std::cout << "Start\n";;
+    std::thread WorkerOne(one);
+    std::thread WorkerTwo(two);
+    std::thread WorkerThree(three);
+    int RaceTimer = 0;
+        while (RaceTimer <= 100)
+        {
+           std::cout << ThingPTR->GetInt() << " " << ThingPTR->GetString() << std::endl;
+           std::this_thread::sleep_for(std::chrono::seconds(1));
+           RaceTimer += 1;
+        }
+        ThingPTR->Timer = true;
+
+        WorkerOne.join();
+        WorkerTwo.join();
+        WorkerThree.join();
+       
 }
