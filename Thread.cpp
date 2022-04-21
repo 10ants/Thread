a57f66e6fe599747b7b7a1e883989bd7f268f381
Jesse Tennant <75157270+BeepBwop@users.noreply.github.com>
2022-04-20 17:09:39 -0700

    Update Thread.cpp

--- Thread.cpp
+++ Thread.cpp
@@ -1,69 +1,58 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include <Windows.h>
+//#include <Windows.h>
 
-class Thing																								  
-{											
-	std::string mStr = " ";
-	int mNum = 0;
+class Thing
+{
+    std::string mStr = " ";
+    int mNum = 0;
 
 public:
-	Thing(int num, std::string string)
-	{
-		mNum = num;
-		mStr = string;
-	}
-	~Thing(){}
 
-	struct Functor
-	{
-		void operator()(Thing* ThingPTR, int count ,std::string ID)
-		{
-			ThingPTR->SetInt(+count);
-			ThingPTR->SetString(ID);
-		}
-	};
+    void operator()(int n, std::string s) //Functor
+    {
+        this->mNum  =n;
+        this->mStr = s;
+    }
 
-	void SetInt(int number) {mNum = number;}
-	int GetInt() {return mNum;}
-	void SetString(std::string string) {mStr = string;}
-	std::string GetString() {return mStr;}
-	
+    void SetInt(int number) {mNum = number;}
+    int GetInt() {return mNum;}
+    void SetString(std::string string) {mStr = string;}
+    std::string GetString() {return mStr;}
+    
 };
 
+void CallFunc(Thing* ThingPTR, int n, std::string s)
+{
+    ThingPTR->operator()(n, s);
+}
 int main()
 {
-	std::cout << "Start\n";
-	int times = 0; //Used to prevent infinite loop
+    std::cout << "Start\n";
+    int ThreeCount = 0;
 
-	while (times < 50)
-	{
-		std::thread WorkerOne;
-		std::thread WorkerTwo;
-		std::thread WorkerThree;
-		int ThreeCount = 0;
-		
-		Thing* ThingPTR = new Thing(0, " ");
-		Thing::Functor()(ThingPTR, 17, "one");
+    while (ThreeCount < INT_MAX)
+    {
+        int ThreeCount = 0;
+        
+        Thing* ThingPTR = new Thing;
+        
+        std::thread WorkerOne(CallFunc, ThingPTR, 17, "One");
+        std::this_thread::yield();
+        std::thread WorkerTwo(CallFunc, ThingPTR, 16, "Two");
+        std::this_thread::yield();
+        std::thread WorkerThree(CallFunc, ThingPTR, ThreeCount, "Three");
+        std::this_thread::yield();
 
-		std::thread WorkerOne(Thing::Functor()(ThingPTR, 17, "one"));
-		std::this_thread::yield();
-		std::thread WorkerTwo(Thing::Functor()(ThingPTR, 13, "two"));
-		std::this_thread::yield();
-		std::thread WorkerThree(Thing::Functor()(ThingPTR, ThreeCount, "three"));
-		std::this_thread::yield();
-
-		for (int i = 0; i < INT_MAX; i++)
-		{
-			ThreeCount = i;
-		}
-		times += 1;
-		Sleep(1);
-
-		std::cout << "Current int value is...." << ThingPTR->GetInt() << std::endl;
-		std::cout << "This was set by thread...." << ThingPTR->GetString() << std::endl;
-	}
-
-	std::cout << "End\n";
+        for (int i = 0; i < INT_MAX; i++)
+        {
+            ThreeCount = i;
+        }
+        
+        //Sleep(1);
+        std::cout << "Current int value is...." << ThingPTR->GetInt() << std::endl;
+        std::cout << "This was set by thread...." << ThingPTR->GetString() << std::endl;
+        std::cout << "End"<<std::endl;
+    }
 }
